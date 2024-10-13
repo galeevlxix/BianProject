b5acc8c546a9e621070629491020a2463ec9a2d5
Galeev LXIX <gtimur477@gmail.com>
2024-10-13 13:28:01 +0300

    Добавьте файлы проекта.

---
+++ BianGame/pch.h
@@ -0,0 +1,7 @@
+// Предварительная компиляция для более быстрого запуска
+#pragma once
+#include <Windows.h>
+#include "resource.h"
+
+#define MAX_NAME_STRING 256
+#define HInstance() GetModuleHandle(NULL)
